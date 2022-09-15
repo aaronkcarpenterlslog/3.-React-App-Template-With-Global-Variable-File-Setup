37a6f53f3ef0309a9510363bf1160186e5f38163
aaronkcarpenterlslog <acarpenter@lslog.com>
2022-09-15 17:53:37 -0500

    Config file added for Env Variables, Test Request Setup in Login Component

--- src/features/Login/Login.js
+++ src/features/Login/Login.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Login = () => {
-  return <div>Login Feature</div>;
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [sampleData, setSampleData] = useState();
+  const [ipAddress, setIpAddress] = useState("");
+
+  useEffect(() => {
+    const testFetch = async () => {
+      try {
+        const response = await fetch(process.env.REACT_APP_SAMPLE_API);
+        if (!response.ok) throw Error("Didn't Return Expected Data");
+        const responseJSON = await response.json();
+        setSampleData(responseJSON);
+        setIpAddress(responseJSON.ip);
+        setFetchError(null);
+      } catch (e) {
+        setFetchError(e.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    setTimeout(() => {
+      (async () => await testFetch())();
+    }, 2000);
+  }, []);
+
+  return (
+    <div>
+      {isLoading && <h3>Loading Data...</h3>}
+      {fetchError && <h4 style={{ color: "red" }}>{`Error: ${fetchError}`}</h4>}
+      <h2>Login Feature</h2>
+      <h2>
+        CURRENT IP ADDRESS:
+        <p>{ipAddress}</p>
+      </h2>
+    </div>
+  );
 };
 
 export default Login;
